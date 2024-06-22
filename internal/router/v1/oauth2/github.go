837af42fc4571b36ad9577ab8baf9f01acc12797
centonhuang <centonhuang@tencent.com>
2024-06-23 00:28:31 +0800

    ✨ feat(api): support user auth

--- internal/router/v1/oauth2/github.go
+++ internal/router/v1/oauth2/github.go
@@ -2,9 +2,16 @@
 package oauth2
 
 import (
+	"context"
+	"encoding/json"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
+	"github.com/hcd233/Aris-AI-go/internal/auth"
 	"github.com/hcd233/Aris-AI-go/internal/config"
 	"github.com/hcd233/Aris-AI-go/internal/protocol"
+	"github.com/hcd233/Aris-AI-go/internal/resource/database/model"
+	"github.com/samber/lo"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
 )
@@ -20,7 +27,10 @@ var githubOauthConfig = &oauth2.Config{
 // HandleGithubLogin handles the Github login.
 func handleGithubLogin(c *gin.Context) {
 	url := githubOauthConfig.AuthCodeURL(config.Oauth2StateString, oauth2.AccessTypeOffline)
-	c.Redirect(302, url)
+	c.JSON(200, protocol.Response{
+		Data:   map[string]interface{}{"url": url},
+		Status: protocol.SUCCESS,
+	})
 }
 
 // HandleGithubCallback handles the Github callback.
@@ -44,12 +54,55 @@ func handleGithubCallback(c *gin.Context) {
 		return
 	}
 
-	data := map[string]interface{}{
-		"token": token,
+	data, err := getGithubUserInfo(token)
+	if err != nil {
+		c.JSON(500, protocol.Response{
+			Message: err.Error(),
+			Status:  protocol.FAILED,
+		})
+		return
 	}
 
+	platform := model.PlatformGithub
+	bindID := strconv.FormatFloat(data["id"].(float64), 'f', -1, 64)
+	user, err := model.QueryUserByPlatformAndID(platform, bindID)
+	if err != nil {
+		c.JSON(400, protocol.Response{
+			Message: err.Error(),
+			Status:  protocol.ERROR,
+		})
+		return
+	}
+	if user != nil {
+		// 如果已有用户，刷新最后登陆时间
+		lo.Must0(user.SetLastLoginTime())
+	} else {
+		// 新用户，保存信息
+		userName, avatar := data["login"].(string), data["avatar_url"].(string)
+		permission := model.PermissionGeneral
+		user = lo.Must(model.AddUserByBasicInfo(userName, avatar, permission, platform, bindID))
+	}
+
+	tokenString := lo.Must(auth.EncodeToken(user.ID))
 	c.JSON(200, protocol.Response{
-		Data:   data,
-		Status: protocol.SUCCESS,
+		Data: map[string]interface{}{
+			"token": tokenString,
+		},
 	})
+}
+
+func getGithubUserInfo(token *oauth2.Token) (map[string]interface{}, error) {
+	client := githubOauthConfig.Client(context.Background(), token)
+	resp, err := client.Get("https://api.github.com/user")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var data map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 
 	"github.com/hcd233/Aris-AI-go/internal/config"
 	"github.com/hcd233/Aris-AI-go/internal/logger"
+	"github.com/hcd233/Aris-AI-go/internal/resource/database"
+	"github.com/hcd233/Aris-AI-go/internal/resource/database/model"
 	"github.com/hcd233/Aris-AI-go/internal/router"
+	"github.com/samber/lo"
 )
 
 func main() {
 	logger.InitLogger()
+	logger.Logger.Info("[logger] init logger successfully")
+	database.InitDatabase()
+	lo.Must0(database.DB.AutoMigrate(model.Models...))
+	logger.Logger.Info("[database] init database successfully")
 	router.InitRouter(router.Router)
 
 	s := &http.Server{
