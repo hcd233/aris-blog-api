f2a8be878171a80ac76e7f10c003028ecd6f7fc0
centonhuang <centonhuang@tencent.com>
2024-06-23 00:27:01 +0800

    ✨ feat(aris-ai): support jwt and mysql config

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,26 +9,73 @@ import (
 )
 
 var (
-	// Port is the port key.
+	// Port string is the port key.
+	//	@update 2024-06-22 08:59:43
 	Port string
-	// ReadTimeout is the read timeout key.
+
+	// ReadTimeout time is the read timeout key.
+	//	@update 2024-06-22 08:59:40
 	ReadTimeout time.Duration
-	// WriteTimeout is the write timeout key.
+
+	// WriteTimeout time is the write timeout key.
+	//	@update 2024-06-22 08:59:37
 	WriteTimeout time.Duration
-	// MaxHeaderBytes is the max header bytes key.
+
+	// MaxHeaderBytes int is the max header bytes key.
+	//	@update 2024-06-22 08:59:34
 	MaxHeaderBytes int
-	// LogLevel is the log level key.
+
+	// LogLevel string is the log level key.
+	//	@update 2024-06-22 08:59:29
 	LogLevel string
-	// LogDirPath is the log directory key.
+
+	// LogDirPath string is the log directory key.
+	//	@update 2024-06-22 08:59:26
 	LogDirPath string
-	// Oauth2GithubClientID is the Github client ID key.
+
+	// Oauth2GithubClientID string is the Github client ID key.
+	//	@update 2024-06-22 08:59:22
 	Oauth2GithubClientID string
-	// Oauth2GithubClientSecret is the Github client secret key.
+
+	// Oauth2GithubClientSecret string
+	//	@update 2024-06-22 08:59:17
 	Oauth2GithubClientSecret string
-	// Oauth2StateString is the OAuth2 state string key.
+
+	// Oauth2StateString string is the OAuth2 state string key.
+	//	@update 2024-06-22 08:59:11
 	Oauth2StateString string
-	// Oauth2GithubRedirectURL is the Github redirect URL key.
+
+	// Oauth2GithubRedirectURL string
+	//	@update 2024-06-22 08:59:07
 	Oauth2GithubRedirectURL string
+
+	// MysqlUser string
+	//	@update 2024-06-22 09:00:30
+	MysqlUser string
+
+	// MysqlPassword string
+	//	@update 2024-06-22 09:00:45
+	MysqlPassword string
+
+	// MysqlHost string
+	//	@update 2024-06-22 09:01:02
+	MysqlHost string
+
+	// MysqlPort string
+	//	@update 2024-06-22 09:01:18
+	MysqlPort string
+
+	// MysqlDatabase string
+	//	@update 2024-06-22 09:01:34
+	MysqlDatabase string
+
+	// JwtTokenExpired int
+	//	@update 2024-06-22 11:09:19
+	JwtTokenExpired int
+
+	// JwtTokenSecret string 
+	//	@update 2024-06-22 11:15:55 
+	JwtTokenSecret string
 )
 
 func init() {
@@ -59,6 +106,15 @@ func initEnvironment() {
 	Oauth2GithubClientSecret = config.GetString("oauth2.github.client.secret")
 	Oauth2StateString = config.GetString("oauth2.state.string")
 	Oauth2GithubRedirectURL = config.GetString("oauth2.github.redirect.url")
+
+	MysqlUser = config.GetString("mysql.user")
+	MysqlPassword = config.GetString("mysql.password")
+	MysqlHost = config.GetString("mysql.host")
+	MysqlPort = config.GetString("mysql.port")
+	MysqlDatabase = config.GetString("mysql.database")
+
+	JwtTokenExpired = config.GetInt("jwt.token.expired")
+	JwtTokenSecret = config.GetString("jwt.token.secret")
 
 	if Oauth2GithubClientID == "" {
 		panic("oauth2.github.client.id is required")
