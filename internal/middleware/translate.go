bd0c05d9234d2936981fb7a6fe3bc718b7fe31e6
centonhuang <centonhuang@tencent.com>
2024-09-16 17:25:23 +0800

    ✨ feat(middleware): add cors and translate

---
+++ internal/middleware/translate.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hcd233/Aris-AI-go/internal/protocol"
+	"github.com/samber/lo"
+)
+
+type responseBodyWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (r responseBodyWriter) Write(b []byte) (int, error) {
+	return r.body.Write(b)
+}
+
+// TranslateMiddleware 翻译状态码中间件
+//
+//	@return gin.HandlerFunc
+//	@author centonhuang
+//	@update 2024-09-16 03:27:12
+func TranslateMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		w := &responseBodyWriter{body: &bytes.Buffer{}, ResponseWriter: c.Writer}
+		c.Writer = w
+
+		c.Next()
+
+		response := protocol.Response{}
+		lo.Must0(json.Unmarshal(w.body.Bytes(), &response))
+
+		code := response.Code
+		if code != 0 {
+			if message, ok := protocol.CodeMessageMapping[code]; ok {
+				response.Message = message
+			} else {
+				response.Message = "未知错误"
+			}
+		}
+
+		translatedResponse := lo.Must1(json.Marshal(response))
+		w.ResponseWriter.Write(translatedResponse)
+		w.body.Reset()
+
+		c.JSON(c.Writer.Status(), translatedResponse)
+	}
+}
