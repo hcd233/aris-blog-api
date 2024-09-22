f277ba0064b26c2d96a1780c2782c43957b34fbd
centonhuang <centonhuang@tencent.com>
2024-09-22 16:22:23 +0800

    🦄 refactor(protocol): refactor article & user protocols

--- internal/protocol/user.go
+++ internal/protocol/user.go
@@ -8,15 +8,6 @@ type UserURI struct {
 	UserName string `uri:"userName" binding:"required"`
 }
 
-// ArticleURI 文章路径参数
-//
-//	@author centonhuang
-//	@update 2024-09-21 06:13:15
-type ArticleURI struct {
-	UserURI
-	ArticleSlug string `uri:"articleSlug" binding:"required"`
-}
-
 // PageParams 列表参数
 //
 //	@author centonhuang
@@ -41,23 +32,4 @@ type QueryParams struct {
 //	@update 2024-09-18 02:39:31
 type UpdateUserBody struct {
 	UserName string `json:"userName" binding:"required"`
-}
-
-// CreateArticleBody 创建文章请求体
-//
-//	@author centonhuang
-//	@update 2024-09-21 09:59:55
-type CreateArticleBody struct {
-	Title string `json:"title" binding:"required"`
-	Slug  string `json:"slug"`
-	// TODO support tags
-}
-
-// UpdateArticleBody 更新文章请求体
-//
-//	@author centonhuang
-//	@update 2024-09-22 03:56:09
-type UpdateArticleBody struct {
-	Title string `json:"title"`
-	Slug  string `json:"slug"`
 }
