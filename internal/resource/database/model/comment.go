f75db8331f68a6ecda82013b15f4b0a88e875fdb
centonhuang <centonhuang@tencent.com>
2024-10-24 17:55:17 +0800

    ✨ feat(aris): support create comment

--- internal/resource/database/model/comment.go
+++ internal/resource/database/model/comment.go
@@ -12,7 +12,7 @@ type Comment struct {
 	ArticleID uint      `json:"article_id" gorm:"column:article_id;not null;comment:'文章ID'"`
 	UserID    uint      `json:"user_id" gorm:"column:user_id;not null;comment:'用户ID'"`
 	Content   string    `json:"content" gorm:"column:content;not null;comment:'评论内容'"`
-	ParentID  uint      `json:"parent_id" gorm:"column:parent_id;default:0;comment:'父评论ID'"`
+	ParentID  uint      `json:"parent_id" gorm:"column:parent_id;default:NULL;comment:'父评论ID'"`
 	Likes     uint      `json:"likes" gorm:"column:likes;default:0;comment:'点赞数'"`
 	Parent    *Comment  `json:"parent" gorm:"foreignKey:ParentID"`
 	Children  []Comment `json:"children" gorm:"foreignKey:ParentID"`
@@ -23,12 +23,14 @@ type Comment struct {
 //	@receiver c *Comment
 //	@return map
 //	@author centonhuang
-//	@update 2024-10-23 07:17:26
+//	@update 2024-10-24 05:45:04
 func (c *Comment) GetBasicInfo() map[string]interface{} {
 	return map[string]interface{}{
-		"id":      c.ID,
-		"userID":  c.UserID,
-		"content": c.Content,
-		"likes":   c.Likes,
+		"id":        c.ID,
+		"userID":    c.UserID,
+		"createdAt": c.CreatedAt,
+		"parentID":  c.ParentID,
+		"content":   c.Content,
+		"likes":     c.Likes,
 	}
 }
---
+++ internal/router/v1/comment/create.go
@@ -0,0 +1,95 @@
+package comment
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hcd233/Aris-blog/internal/protocol"
+	"github.com/hcd233/Aris-blog/internal/resource/database"
+	"github.com/hcd233/Aris-blog/internal/resource/database/dao"
+	"github.com/hcd233/Aris-blog/internal/resource/database/model"
+	"github.com/samber/lo"
+)
+
+// CreateArticleCommentHandler 创建文章评论
+//
+//	@param c *gin.Context
+//	@author centonhuang
+//	@update 2024-10-24 04:29:47
+func CreateArticleCommentHandler(c *gin.Context) {
+	userID := c.MustGet("userID").(uint)
+	uri := c.MustGet("uri").(*protocol.ArticleURI)
+	body := c.MustGet("body").(*protocol.CreateArticleCommentBody)
+
+	db := database.GetDBInstance()
+
+	userDAO, articleDAO, commentDAO := dao.GetUserDAO(), dao.GetArticleDAO(), dao.GetCommentDAO()
+
+	author, err := userDAO.GetByName(db, uri.UserName, []string{"id"})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, protocol.Response{
+			Code:    protocol.CodeGetUserError,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	article, err := articleDAO.GetBySlugAndUserID(db, uri.ArticleSlug, author.ID, []string{"id", "status"})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, protocol.Response{
+			Code:    protocol.CodeGetArticleError,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	if article.Status == model.ArticleStatusDraft {
+		c.JSON(http.StatusForbidden, protocol.Response{
+			Code:    protocol.CodeNotPermissionError,
+			Message: "How did you find this article?",
+		})
+		return
+	}
+
+	var parent *model.Comment
+	if body.ReplyTo != 0 {
+		parent, err = commentDAO.GetByID(db, body.ReplyTo, []string{"id", "article_id"})
+		if err != nil {
+			c.JSON(http.StatusBadRequest, protocol.Response{
+				Code:    protocol.CodeGetCommentError,
+				Message: err.Error(),
+			})
+			return
+		}
+
+		if parent.ArticleID != article.ID {
+			c.JSON(http.StatusBadRequest, protocol.Response{
+				Code:    protocol.CodeURIError,
+				Message: "The parent comment is not in this article",
+			})
+			return
+		}
+	}
+
+	comment := &model.Comment{
+		UserID:    userID,
+		ArticleID: article.ID,
+		Parent:    parent,
+		Content:   body.Content,
+	}
+
+	if err := commentDAO.Create(db, comment); err != nil {
+		c.JSON(http.StatusBadRequest, protocol.Response{
+			Code:    protocol.CodeCreateCommentError,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	comment = lo.Must1(commentDAO.GetByID(db, comment.ID, []string{"id", "created_at", "content", "parent_id"}))
+
+	c.JSON(http.StatusOK, protocol.Response{
+		Code: protocol.CodeOk,
+		Data: comment.GetBasicInfo(),
+	})
+}
