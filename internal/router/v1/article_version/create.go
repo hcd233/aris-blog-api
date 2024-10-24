87e187967cfb772791772aaf1aa63b14b577d42a
centonhuang <centonhuang@tencent.com>
2024-10-24 14:59:21 +0800

    🐞 fix(aris): fix bugs

--- internal/router/v1/article_version/create.go
+++ internal/router/v1/article_version/create.go
@@ -3,6 +3,7 @@ package version
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"gorm.io/gorm"
 
@@ -11,6 +12,10 @@ import (
 	"github.com/hcd233/Aris-blog/internal/resource/database"
 	"github.com/hcd233/Aris-blog/internal/resource/database/dao"
 	"github.com/hcd233/Aris-blog/internal/resource/database/model"
+	"github.com/hcd233/Aris-blog/internal/resource/search"
+	docdao "github.com/hcd233/Aris-blog/internal/resource/search/doc_dao"
+	"github.com/hcd233/Aris-blog/internal/resource/search/document"
+	"github.com/samber/lo"
 )
 
 // CreateArticleVersionHandler 创建文章版本
@@ -44,7 +49,7 @@ func CreateArticleVersionHandler(c *gin.Context) {
 		return
 	}
 
-	article, err := articleDAO.GetBySlugAndUserID(db, uri.ArticleSlug, user.ID, []string{"id"})
+	article, err := articleDAO.GetBySlugAndUserID(db, uri.ArticleSlug, user.ID, []string{"id", "status"})
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, protocol.Response{
 			Code:    protocol.CodeGetArticleError,
@@ -81,6 +86,21 @@ func CreateArticleVersionHandler(c *gin.Context) {
 			Code: protocol.CodeCreateArticleVersionError,
 		})
 		return
+	}
+
+	if article.Status == model.ArticleStatusPublish {
+		searchEngine := search.GetSearchEngine()
+		articleDocDAO := docdao.GetArticleDocDAO()
+
+		lo.Must0(articleDocDAO.UpdateDocument(searchEngine, document.TransformArticleToDocument(&model.Article{ID: article.ID, User: &model.User{}}, articleVersion)))
+
+		if err := articleDAO.Update(db, article, map[string]interface{}{"published_at": time.Now()}); err != nil {
+			c.JSON(http.StatusBadRequest, protocol.Response{
+				Code:    protocol.CodeUpdateArticleError,
+				Message: err.Error(),
+			})
+			return
+		}
 	}
 
 	c.JSON(http.StatusOK, protocol.Response{
