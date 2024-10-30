0055a156fb9b9cc1715a8c9d0a00e65a37efbf33
centonhuang <centonhuang@tencent.com>
2024-10-30 20:51:42 +0800

    🦄 refactor(aris): tiny refactor

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
-// Description: Main entry point for the Aris AI Go application.
+// Package main Main entry point for the Aris AI Go application.
+//	@update 2024-10-30 08:50:53 
 package main
 
 import (
