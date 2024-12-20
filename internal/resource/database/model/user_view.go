549226968a075c7e62b77dca7e6a9bbc00ad975d
centonhuang <centonhuang@tencent.com>
2024-12-20 23:22:24 +0800

    🐞 fix: tiny fix

--- internal/resource/database/model/user_view.go
+++ internal/resource/database/model/user_view.go
@@ -16,7 +16,7 @@ type UserView struct {
 	User         *User     `json:"user" gorm:"foreignKey:UserID;references:ID"`
 	ArticleID    uint      `json:"article_id" gorm:"not null;index:user_id_object_type;uniqueIndex:user_object;comment:文章ID"`
 	Article      *Article  `json:"article" gorm:"foreignKey:ArticleID;references:ID"`
-	LastViewedAt time.Time `json:"last_viewed_at" gorm:"not null;default:CURRENT_TIMESTAMP;comment:最后浏览时间"`
+	LastViewedAt time.Time `json:"last_viewed_at" gorm:"not null;comment:最后浏览时间"`
 	Progress     int8      `json:"progress" gorm:"not null;comment:浏览进度"`
 }
 
