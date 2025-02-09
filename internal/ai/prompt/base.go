b75490ebf47a57e59663e91e28e7136e43750a38
centonhuang <centonhuang@tencent.com>
2025-02-09 21:51:46 +0800

    🦄 refactor: remove ai pkg

--- internal/ai/prompt/base.go
+++
@@ -1,9 +0,0 @@
-package prompt
-
-import (
-	"github.com/hcd233/aris-blog-api/internal/ai/message"
-)
-
-type Prompt interface {
-	Format(params map[string]interface{}) (messages []message.Message, err error)
-}
