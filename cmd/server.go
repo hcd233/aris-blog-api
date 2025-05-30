ad2673bf7aea9c17dc42fcf48ce9c1298da4cdbc
centonhuang <centonhuang@tencent.com>
2025-05-31 01:34:14 +0800

    ✨ feat: support trace middleware

--- cmd/server.go
+++ cmd/server.go
@@ -42,7 +42,8 @@ var startServerCmd = &cobra.Command{
 
 		r := gin.New()
 		r.Use(
-			middleware.LogMiddleware(logger.Logger()),
+			middleware.TraceMiddleware(),
+			middleware.LogMiddleware(),
 			ginzap.RecoveryWithZap(logger.Logger(), true),
 			gzip.Gzip(gzip.DefaultCompression, gzip.WithExcludedExtensions(
 				[]string{".pdf", ".mp3", ".wav", ".ogg", ".mov", ".weba", ".mkv", ".mp4", ".webm", ".flac"},
