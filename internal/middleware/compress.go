75198d42646e30eedd36e730bef1e13ce69f3ddd
centonhuang <centonhuang@tencent.com>
2025-11-07 16:43:33 +0800

    🦄 refactor: update compression level to BestSpeed

--- internal/middleware/compress.go
+++ internal/middleware/compress.go
@@ -12,6 +12,6 @@ import (
 //	@update 2025-08-18 20:22:36
 func CompressMiddleware() fiber.Handler {
 	return compress.New(compress.Config{
-		Level: compress.LevelDefault,
+		Level: compress.LevelBestSpeed,
 	})
 }
